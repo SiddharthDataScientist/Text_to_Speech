fd393d8dab96d1bbbbb280341dad09bb3f35df4c
SiddharthDataScientist <siddharthpujari108@gmail.com>
2023-06-28 11:05:20 +0530

    Updated code

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,8 +1,18 @@
-import gtts
-import playsound
+from fastapi import FastAPI
+from  gtts import gTTS
+from fastapi.responses import FileResponse
+import uvicorn
 
-text = input('Enter the text you want to convert: ')
-speech = gtts.gTTS(text= text, lang='en')
+app = FastAPI()
 
-speech.save("Audio.mp3")
-playsound.playsound('Audio.mp3')
+@app.get("/convert")
+def convert_text_to_speech(text: str):
+    speech = gTTS(text=text, lang='en')
+    audio_file = "Audio.mp3"
+    speech.save(audio_file)
+    return FileResponse(audio_file)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9999)
+
+
